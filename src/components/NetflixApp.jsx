42b22802e798446fe3d5b1542c85441e161eb47c
Wil-dev <mehdididou7@icloud.com>
2024-12-13 19:01:18 +0100

    films et series aleatoire ds le header

--- src/components/NetflixApp.jsx
+++ src/components/NetflixApp.jsx
@@ -1,66 +1,70 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import { NetflixAppBar } from "./NetflixAppBar";
-import { NetflixRow } from "./NetflixRow";
-import { NetflixFooter } from "./NetflixFooter";
-import "./Netflix.css";
-import axios from "axios";
+import React from 'react'
+import {NetflixAppBar} from './NetflixAppBar'
+import {NetflixRow} from './NetflixRow'
+import {NetflixFooter} from './NetflixFooter'
+import './Netflix.css'
+import axios from 'axios'
+import {getRandomIntInclusive} from '../utils/helper'
 
-const NetflixHeader = ({ movie }) => {
-	// 🐶 si 'movie' n'est pas défini, retourne un fragment vide 🤖 <></>
+const NetflixHeader = ({movie, type = 'movie'}) => {
+  const title = type === 'movie' ? movie?.title : movie?.name
+  const imageUrl = `https://image.tmdb.org/t/p/original${movie?.backdrop_path}`
+  const banner = {
+    backgroundImage: `url('${imageUrl}')`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    color: 'white',
+    objectFit: 'contain',
+    height: '448px',
+  }
+  if (!movie) {
+    return <></>
+  }
 
-	// 🐶 si 'movie' est défini, retourne le header
-	const imageUrl = `https://image.tmdb.org/t/p/original${movie?.backdrop_path}`;
-	const banner = {
-		backgroundImage: `url('${imageUrl}')`,
-		backgroundSize: "cover",
-		backgroundPosition: "center center",
-		color: "white",
-		objectFit: "contain",
-		height: "450px",
-	};
-	if (!movie) {
-		return <></>;
-	}
-
-	return (
-		<header style={banner}>
-			<div className="banner__contents">
-				<h1 className="banner__title">{movie?.title ?? "..."}</h1>
-				<div className="banner__buttons">
-					<button className="banner__button banner__buttonplay">Lecture</button>
-					<button className="banner__button banner__buttonInfo">
-						Ajouter à ma liste
-					</button>
-				</div>
-				<h1 className="synopsis">{movie?.overview ?? "..."}</h1>
-			</div>
-			<div className="banner--fadeBottom"></div>
-		</header>
-	);
-};
+  return (
+    <header style={banner}>
+      <div className="banner__contents">
+        <h1 className="banner__title">{title ?? '...'}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button banner__buttonplay">Lecture</button>
+          <button className="banner__button banner__buttonInfo">
+            Ajouter à ma liste
+          </button>
+        </div>
+        <h1 className="synopsis">{movie?.overview ?? '...'}</h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  )
+}
 
 const NetflixApp = () => {
-	const [headerMovie, setHeaderMovie] = React.useState();
-	const defaultMovieId = 399566;
-	const apiKey = "4fc7b001e8a107fe1fddc6b41ed0f4af";
-	const lang = "fr-fr";
-	const url = `https://api.themoviedb.org/3/movie/${defaultMovieId}?api_key=${apiKey}&language=${lang}`;
-	React.useEffect(() => {
-		axios
-			.get(url)
-			.then((response) => setHeaderMovie(response))
-			.catch((error) => console.error(error));
-	}, [url]);
+  const [headerMovie, setHeaderMovie] = React.useState()
+  const [type] = React.useState(['movie', 'tv'][getRandomIntInclusive(0, 1)])
+  const tvIds = [71446, 60574, 1399, 66732]
+  const moviesIds = [399566, 602734, 579047, 385128, 615658]
+  const tvId = tvIds[getRandomIntInclusive(0, tvIds.length - 1)]
+  const movieId = moviesIds[getRandomIntInclusive(0, moviesIds.length - 1)]
+  const defaultMovieId = type === 'movie' ? movieId : tvId
+  const apiKey = '4fc7b001e8a107fe1fddc6b41ed0f4af'
+  const lang = 'fr-fr'
+  const url = `https://api.themoviedb.org/3/${type}/${defaultMovieId}?api_key=${apiKey}&language=${lang}`
+  React.useEffect(() => {
+    axios
+      .get(url)
+      .then(response => setHeaderMovie(response))
+      .catch(error => console.error(error))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-	return (
-		<div>
-			<NetflixAppBar />
-			<NetflixHeader movie={headerMovie?.data} />
-			<NetflixRow wideImage={false} title="Films Netflix" />
-			<NetflixRow wideImage={true} title="Série Netflix" />
-			<NetflixFooter />
-		</div>
-	);
-};
-export { NetflixApp };
+  return (
+    <div>
+      <NetflixAppBar />
+      <NetflixHeader movie={headerMovie?.data} type={type} />
+      <NetflixRow wideImage={false} title="Films Netflix" />
+      <NetflixRow wideImage={true} title="Série Netflix" />
+      <NetflixFooter />
+    </div>
+  )
+}
+export {NetflixApp}
