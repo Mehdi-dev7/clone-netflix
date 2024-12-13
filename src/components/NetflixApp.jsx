31930e04169511358a7b5708aa91cc2e77235786
Wil-dev <mehdididou7@icloud.com>
2024-12-13 20:27:23 +0100

    ajout du fichier helper & config.js ,affichage films aleatoire

--- src/components/NetflixApp.jsx
+++ src/components/NetflixApp.jsx
@@ -1,70 +1,71 @@
-import React from 'react'
-import {NetflixAppBar} from './NetflixAppBar'
-import {NetflixRow} from './NetflixRow'
-import {NetflixFooter} from './NetflixFooter'
-import './Netflix.css'
-import axios from 'axios'
-import {getRandomIntInclusive} from '../utils/helper'
+import React from "react";
+import { NetflixAppBar } from "./NetflixAppBar";
+import { NetflixRow } from "./NetflixRow";
+import { NetFlixFooter } from "./NetFlixFooter";
+import "./Netflix.css";
+import axios from "axios";
+import { getRandomId, getRandomType } from "../utils/helper";
+import {
+	imagePathOriginal,
+	TYPE_MOVIE,
+	apiKey,
+	lang,
+	API_URL,
+} from "../config";
 
-const NetflixHeader = ({movie, type = 'movie'}) => {
-  const title = type === 'movie' ? movie?.title : movie?.name
-  const imageUrl = `https://image.tmdb.org/t/p/original${movie?.backdrop_path}`
-  const banner = {
-    backgroundImage: `url('${imageUrl}')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    color: 'white',
-    objectFit: 'contain',
-    height: '448px',
-  }
-  if (!movie) {
-    return <></>
-  }
+const NetflixHeader = ({ movie, type = TYPE_MOVIE }) => {
+	const title = type === TYPE_MOVIE ? movie?.title : movie?.name;
+	const imageUrl = `${imagePathOriginal}${movie?.backdrop_path}`;
+	const banner = {
+		backgroundImage: `url('${imageUrl}')`,
+		backgroundSize: "cover",
+		backgroundPosition: "center center",
+		color: "white",
+		objectFit: "contain",
+		height: "448px",
+	};
+	if (!movie) {
+		return <></>;
+	}
 
-  return (
-    <header style={banner}>
-      <div className="banner__contents">
-        <h1 className="banner__title">{title ?? '...'}</h1>
-        <div className="banner__buttons">
-          <button className="banner__button banner__buttonplay">Lecture</button>
-          <button className="banner__button banner__buttonInfo">
-            Ajouter à ma liste
-          </button>
-        </div>
-        <h1 className="synopsis">{movie?.overview ?? '...'}</h1>
-      </div>
-      <div className="banner--fadeBottom"></div>
-    </header>
-  )
-}
+	return (
+		<header style={banner}>
+			<div className="banner__contents">
+				<h1 className="banner__title">{title ?? "..."}</h1>
+				<div className="banner__buttons">
+					<button className="banner__button banner__buttonplay">Lecture</button>
+					<button className="banner__button banner__buttonInfo">
+						Ajouter à ma liste
+					</button>
+				</div>
+				<h1 className="synopsis">{movie?.overview ?? "..."}</h1>
+			</div>
+			<div className="banner--fadeBottom"></div>
+		</header>
+	);
+};
 
 const NetflixApp = () => {
-  const [headerMovie, setHeaderMovie] = React.useState()
-  const [type] = React.useState(['movie', 'tv'][getRandomIntInclusive(0, 1)])
-  const tvIds = [71446, 60574, 1399, 66732]
-  const moviesIds = [399566, 602734, 579047, 385128, 615658]
-  const tvId = tvIds[getRandomIntInclusive(0, tvIds.length - 1)]
-  const movieId = moviesIds[getRandomIntInclusive(0, moviesIds.length - 1)]
-  const defaultMovieId = type === 'movie' ? movieId : tvId
-  const apiKey = '4fc7b001e8a107fe1fddc6b41ed0f4af'
-  const lang = 'fr-fr'
-  const url = `https://api.themoviedb.org/3/${type}/${defaultMovieId}?api_key=${apiKey}&language=${lang}`
-  React.useEffect(() => {
-    axios
-      .get(url)
-      .then(response => setHeaderMovie(response))
-      .catch(error => console.error(error))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+	const [headerMovie, setHeaderMovie] = React.useState();
+	const [type] = React.useState(getRandomType());
+	const defaultMovieId = getRandomId(type);
+	const url = `https://api.themoviedb.org/3/${type}/${defaultMovieId}?api_key=${apiKey}&language=${lang}`;
+	React.useEffect(() => {
+		axios
+			.get(url)
+			.then((response) => setHeaderMovie(response))
+			.catch((error) => console.error(error));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <div>
-      <NetflixAppBar />
-      <NetflixHeader movie={headerMovie?.data} type={type} />
-      <NetflixRow wideImage={false} title="Films Netflix" />
-      <NetflixRow wideImage={true} title="Série Netflix" />
-      <NetflixFooter />
-    </div>
-  )
-}
-export {NetflixApp}
+	return (
+		<div>
+			<NetflixAppBar />
+			<NetflixHeader movie={headerMovie?.data} type={type} />
+			<NetflixRow wideImage={false} title="Films Netflix" />
+			<NetflixRow wideImage={true} title="Série Netflix" />
+			<NetFlixFooter />
+		</div>
+	);
+};
+export { NetflixApp };
--- src/components/NetflixFooter.jsx
+++ src/components/NetflixFooter.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const NetflixFooter = () => {
+const NetFlixFooter = () => {
 	return <footer className="footer">2021 - Netflix Clone</footer>;
 };
-export { NetflixFooter };
+export { NetFlixFooter };
