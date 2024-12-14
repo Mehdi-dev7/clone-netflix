692ca6e0cb79368de33fffcf0c52c1035b90ff59
Wil-dev <mehdididou7@icloud.com>
2024-12-14 18:14:16 +0100

    add useFetchData & component NetflixHeader

--- src/components/NetflixApp.jsx
+++ src/components/NetflixApp.jsx
@@ -1,71 +1,61 @@
-import React from "react";
-import { NetflixAppBar } from "./NetflixAppBar";
-import { NetflixRow } from "./NetflixRow";
-import { NetFlixFooter } from "./NetFlixFooter";
-import "./Netflix.css";
-import axios from "axios";
-import { getRandomId, getRandomType } from "../utils/helper";
-import {
-	imagePathOriginal,
-	TYPE_MOVIE,
-	apiKey,
-	lang,
-	API_URL,
-} from "../config";
-import { clientApi } from "../utils/clientApi";
+import React from 'react'
+import {NetflixAppBar} from './NetflixAppBar'
+import {NetflixRow} from './NetflixRow'
+import {NetFlixFooter} from './NetFlixFooter'
+import {NetflixHeader} from './NetflixHeader'
+import {getRandomType, getRandomId} from '../utils/helper'
+import {clientApi} from '../utils/clientApi'
+import {makeStyles} from '@mui/styles'
+import {Alert, AlertTitle} from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress'
+import {useFetchData} from '../utils/hooks'
+import './Netflix.css'
 
-const NetflixHeader = ({ movie, type = TYPE_MOVIE }) => {
-	const title = type === TYPE_MOVIE ? movie?.title : movie?.name;
-	const imageUrl = `${imagePathOriginal}${movie?.backdrop_path}`;
-	const banner = {
-		backgroundImage: `url('${imageUrl}')`,
-		backgroundSize: "cover",
-		backgroundPosition: "center center",
-		color: "white",
-		objectFit: "contain",
-		height: "448px",
-	};
-	if (!movie) {
-		return <></>;
-	}
-
-	return (
-		<header style={banner}>
-			<div className="banner__contents">
-				<h1 className="banner__title">{title ?? "..."}</h1>
-				<div className="banner__buttons">
-					<button className="banner__button banner__buttonplay">Lecture</button>
-					<button className="banner__button banner__buttonInfo">
-						Ajouter à ma liste
-					</button>
-				</div>
-				<h1 className="synopsis">{movie?.overview ?? "..."}</h1>
-			</div>
-			<div className="banner--fadeBottom"></div>
-		</header>
-	);
-};
+const useStyles = makeStyles(theme => ({
+  alert: {
+    width: '50%',
+    margin: 'auto',
+    marginBotton: '50px',
+  },
+  progress: {
+    marginLeft: '30px',
+  },
+}))
 
 const NetflixApp = () => {
-	const [headerMovie, setHeaderMovie] = React.useState();
-	const [type] = React.useState(getRandomType());
-	const defaultMovieId = getRandomId(type);
-	const url = `https://api.themoviedb.org/3/${type}/${defaultMovieId}?api_key=${apiKey}&language=${lang}`;
-	React.useEffect(() => {
-		clientApi(`${type}/${defaultMovieId}`)
-			.then((response) => setHeaderMovie(response))
-			.catch((error) => console.error(error));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  const {data: headerMovie, error, status, execute} = useFetchData()
+  const [type] = React.useState(getRandomType())
+  const defaultMovieId = getRandomId(type)
+  const classes = useStyles()
 
-	return (
-		<div>
-			<NetflixAppBar />
-			<NetflixHeader movie={headerMovie?.data} type={type} />
-			<NetflixRow wideImage={false} title="Films Netflix" />
-			<NetflixRow wideImage={true} title="Série Netflix" />
-			<NetFlixFooter />
-		</div>
-	);
-};
-export { NetflixApp };
+  React.useEffect(() => {
+    execute(clientApi(`${type}/${defaultMovieId}`))
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div>
+      <NetflixAppBar />
+      <NetflixHeader movie={headerMovie?.data} type={type} />
+      <NetflixRow wideImage={false} title="Films Netflix" />
+      <NetflixRow wideImage={true} title="Série Netflix" />
+      <NetFlixFooter />
+
+      {status === 'error' ? (
+        <div className={classes.alert}>
+          <Alert severity="error">
+            <AlertTitle>Une erreur est survenue</AlertTitle>
+            Detail : {error.message}
+          </Alert>
+        </div>
+      ) : null}
+      {status === 'fetching' ? (
+        <div className={classes.progress}>
+          <CircularProgress color="primary" />
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export {NetflixApp}
--- src/utils/clientApi.js
+++ src/utils/clientApi.js
@@ -1,11 +1,14 @@
-import axios from 'axios'
-import {apiKey, lang, API_URL} from '../config'
+import axios from "axios";
+import { apiKey, lang, API_URL } from "../config";
 
-const clientApi = endpoint => {
-  const page = 1
-  const startChar = endpoint.includes('?') ? `&` : `?`
-  const keyLang = `${startChar}api_key=${apiKey}&language=${lang}&page=${page}`
-  return axios.get(`${API_URL}/${endpoint}${keyLang}`)
-}
+const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t));
 
-export {clientApi}
+const clientApi = async (endpoint) => {
+	const page = 1;
+	await sleep(1000);
+	const startChar = endpoint.includes("?") ? `&` : `?`;
+	const keyLang = `${startChar}api_key=${apiKey}&language=${lang}&page=${page}`;
+	return axios.get(`${API_URL}/${endpoint}${keyLang}`);
+};
+
+export { clientApi };
